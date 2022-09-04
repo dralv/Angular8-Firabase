5e0d8012d208ec76adc2e9448fe540521f23b4a8
Alvaro <dralvphd2@gmail.com>
2022-09-04 14:26:02 -0300

    Serviços de Autenticação

--- requisicoes-app/src/app/services/authentication.service.ts
+++ requisicoes-app/src/app/services/authentication.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private user:Observable<firebase.User|null >;
+
+  constructor(private afAuth:AngularFireAuth) {
+    this.user = afAuth.authState
+   }
+
+   login(email:string,password:string):Promise<firebase.auth.UserCredential>{
+    return this.afAuth.signInWithEmailAndPassword(email,password)
+   }
+
+   logout(): Promise<void>{
+    return this.afAuth.signOut();
+   }
+   
+   resetPassword(email:string){
+    return this.afAuth.sendPasswordResetEmail(email)
+   }
 }
